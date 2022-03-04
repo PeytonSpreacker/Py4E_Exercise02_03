29a50a68c7a2f4615ea6e02d8b3339fcda2058a6
Your Name <peyton@spreacker.com>
2022-03-03 20:06:00 -0600

    inserted clarifying comments

--- exercise02_03.py
+++ exercise02_03.py
@@ -1,4 +1,5 @@
 hours = input("Enter Hours: ")
 rate = input("Enter Rate: ")
+#input values come in as strings, we want to change them to floats in order to do math with them. This is done in the equation below using the float() function.
 Pay = float(hours) * float(rate)
 print("Pay:", Pay)
